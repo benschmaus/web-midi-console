cf51eef332b41526b046f38d03231bab1533226b
Ben Schmaus <benjamin.schmaus@gmail.com>
2015-11-19 21:18:58 -0800

    Initial version with support for inputs and outputs with responsive event handlers

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # web-midi-console
-Simple web app that uses the Web MIDI API to display connected MIDI devices and messages sent from them
+Simple web app that uses the Web MIDI API to display connected MIDI devices and messages sent from them.
--- index.js
+++ index.js
@@ -9,35 +9,86 @@ onMIDISuccess: function(midiAccess) {
         self.onMIDIAccessChange(e);
     }
     this.midiAccess = midiAccess;
+    this.inputs = {};
+    this.outputs = {};
+
     this.initPorts();
 },
 
 initPorts: function() {    
     var self = this;
 
-    $("#midiPorts").empty();
-    
-    var html = "";
-    this.midiAccess.inputs.forEach(
-        function(port, key) {
-            
-            console.log(port);            
-            var tmpl = $.templates(
-                "<div>{{:port.name}}</div>"
-            );
-            html += tmpl.render({ "port": port });
+    var inputs = this.midiAccess.inputs;
+    if (inputs.size > 0) {
+        inputs.forEach(
+            function(port, key) {
+                //console.log(port);
+                self.registerPort(port);
+            }
+        );
+    } else {
+        $("#midiinputs").append("<p>No connected inputs</p>");
+    }
 
-            port.onmidimessage = function(m) { self.onMIDIMessage(m); };
-            port.onstatechange = function(e) { self.onPortStateChange(e); };
-        }
-    );
-    $("#midiPorts").html(html);
+    var outputs = this.midiAccess.outputs;
+    if (outputs.size > 0) {
+        outputs.forEach(
+            function(port, key) {
+                self.registerPort(port);        
+                self.renderPort(port);
+            }
+        );
+    } else {
+        $("#midioutputs").append("<p>No connected outputs</p>"); 
+    }
 },
 
 onMIDIAccessChange: function(e) {
     console.log(e);
     //console.log(this);
-    this.initPorts();
+    var port = e.port;
+    var portContainer = $("#midi" + port.type + "s");
+    if (portContainer.html().startsWith("<p>No connected")) {
+        portContainer.empty();
+    }
+
+    if (port.type == "input") {
+        if (this.inputs[port.name] === undefined) {
+            this.registerPort(port);
+        }
+    } else {
+        if (this.outputs[port.name] === undefined) {
+            this.registerPort(port);
+        }
+    }
+
+    this.renderPort(port);
+},
+
+renderPort: function(port) {
+    if (port.state == "connected") {
+        if (!$("#" + port.type + port.id).length) {
+            var tmpl = $.templates(
+                "<div id='{{:type}}{{:port.id}}'>{{:port.name}}</div>"
+            );         
+            var html = tmpl.render({ "port": port, "type": port.type });
+            $("#midi" + port.type + "s").append(html);
+        }
+    } else {
+        $("#" + port.type + port.id).remove();
+    }
+},
+
+registerPort: function(port) {
+    var self = this;
+    if (port.type == "input") {
+        this.inputs[port.name] = port;
+        port.onmidimessage = function(m) { self.onMIDIMessage(m); };
+    } else {
+        this.outputs[port.name] = port;
+    }
+
+    port.onstatechange = function(e) { self.onPortStateChange(e); };
 },
 
 onMIDIFailure: function(e) {
