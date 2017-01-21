e1a1f9fb5e6c03786eba92809f9a49642f6c3389
Ben Schmaus <benjamin.schmaus@gmail.com>
2017-01-21 06:14:23 -0500

    updates

--- user-script-example.js
+++ user-script-example.js
@@ -2,7 +2,7 @@
 userCommands(
   function() {
     var d = device("Livid Minim Bluetooth").ch(15);
-    for (var i = 10; i <= 18; i++) {
+    for (var i = 10; i < 18; i++) {
     	d.cc(i, 42);
     }
   }
